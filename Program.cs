cbc9255065e2473e28a58bc3afb29afc361221f8
mdlaras <52792426+mdlaras@users.noreply.github.com>
2026-05-21 14:48:18 +0200

    create base structure of program

--- Program.cs
+++ Program.cs
@@ -1,1 +1,18 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System.Runtime.InteropServices;
+
+
+public class Converter
+{
+    [DllImport("markdown_tools.dll")]
+    public static extern void generateLatex(string inName, string outName);
+}
+
+public class Program
+{
+    static void Main(string[] arg)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+    
+}
+
